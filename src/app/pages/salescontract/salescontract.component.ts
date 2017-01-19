d653b97a1f472cdc6d3787c328f514c8b7aca2aa
nelson <nelson@sopto.com>
2017-01-19 16:26:03 +0800

    未上传组件文件

---
+++ src/app/pages/salescontract/salescontract.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'salescontract',
+  styles: [],
+  template: `<h1>nelson test</h1>
+  <button class="btn btn-warning" type="button">Warning</button>`
+})
+
+export class SalesContractComponent{
+  constructor(){}
+}
